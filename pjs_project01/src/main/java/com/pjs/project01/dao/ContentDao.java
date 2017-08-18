05395c4a824d154e46867942d7252f1934bae781
soul9862 <soul9862@naver.com>
2017-08-18 20:54:25 +0900

    content dao,dto 추가 입력

--- pjs_project01/src/main/java/com/pjs/project01/dao/ContentDao.java
+++ pjs_project01/src/main/java/com/pjs/project01/dao/ContentDao.java
@@ -1,5 +1,32 @@
 package com.pjs.project01.dao;
 
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.pjs.project01.dto.ContentDto;
+import com.pjs.project01.util.Constant;
+
 public class ContentDao {
 
+	DataSource dataSource;
+	
+	JdbcTemplate template;
+	
+	public ContentDao() {
+		// TODO Auto-generated constructor stub
+		template = Constant.template;
+	}
+	
+	public ArrayList<ContentDto> list(){
+		
+		String query = "select * from movie";
+		
+		return (ArrayList<ContentDto>) template.query(query, new BeanPropertyRowMapper<ContentDto>(ContentDto.class));
+	}
+	
+	
 }
--- pjs_project01/src/main/java/com/pjs/project01/dto/ContentDto.java
+++ pjs_project01/src/main/java/com/pjs/project01/dto/ContentDto.java
@@ -3,5 +3,51 @@ package com.pjs.project01.dto;
 public class ContentDto {
 
 	private Integer code;
+	private String name;
+	private String userid;
+	private String description;;
+	private String pictureUrl;
+	public Integer getCode() {
+		return code;
+	}
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
+	}
+	public ContentDto(Integer code, String name, String userid, String description, String pictureUrl) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.userid = userid;
+		this.description = description;
+		this.pictureUrl = pictureUrl;
+	}
+	
+	public ContentDto() {
+		// TODO Auto-generated constructor stub
+	}
 	
 }
