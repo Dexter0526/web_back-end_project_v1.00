3e5ec6a32954f7cfe05a418df02af67170453370
soul9862 <soul9862@naver.com>
2017-08-23 11:15:20 +0900

    업로드 작성 커맨드

--- pjs_project01/src/main/java/com/pjs/project01/CCommand/CWriteCommand.java
+++ pjs_project01/src/main/java/com/pjs/project01/CCommand/CWriteCommand.java
@@ -1,6 +1,12 @@
 package com.pjs.project01.CCommand;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.ui.Model;
+
+import com.pjs.project01.dao.ContentDao;
 
 public class CWriteCommand implements CCommand {
 
@@ -8,6 +14,18 @@ public class CWriteCommand implements CCommand {
 	public void execute(Model model) {
 		// TODO Auto-generated method stub
 
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		
+		int code = Integer.parseInt(request.getParameter("code"));
+		String title = request.getParameter("title");
+		String pictureUrl = request.getParameter("pictureUrl");
+		String userid = request.getParameter("userid");
+		String description = request.getParameter("description");
+		
+		ContentDao cdao = new ContentDao();
+		cdao.write(code, title, pictureUrl, userid, description);
+		
 	}
 
 }
