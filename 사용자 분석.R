fdb5174c45b8e0f60e6c792ccc2d433d6da977db
soul9862 <soul9862@naver.com>
2017-09-10 21:51:16 +0900

    코드 R 칼럼 수정

--- 사용자 분석.R
+++ 사용자 분석.R
@@ -3,10 +3,16 @@ movie = c(5,17,2,9,5,5,9,8,8,11,6,5,7,8,1,2,2,1,3,13,8,2,0,5,9,1,0)
 variety = c(3,1,13,5,5,1,6,6,2,8,19,5,5,9,10,18,18,1,5,9,12,10,6,8,7,1,1)
 news = c(15,7,12,1,7,5,7,2,8,1,10,13,6,6,10,15,16,16,18,13,8,7,0,3,0,6,2)
 docu = c(4,6,15,8,8,6,8,5,9,6,18,3,2,8,9,14,15,3,6,1,9,9,8,2,10,9,9)
-user_hit = data.frame(movie, variety, news, docu, userid)
+user_hit = data.frame(row.names = userid, movie, variety, news, docu)
 user_hit
 test = data.frame(movie, variety, news, docu)
 test
+
+call_info <- function(userid, user_hit) {
+
+  subset(user_hit, pname==userid)
+
+}
 
 summary(user_hit)
 
@@ -19,14 +25,17 @@ a
 install.packages("Hmisc")
 library(Hmisc)
 
-rcorr(as.matrix(test), type = "spearman")
+rcorr(as.matrix(user_hit), type = "spearman")
 
-pca <- princomp(test, cor = T)
+pca <- princomp(user_hit, cor = T)
 summary(pca)
 biplot(pca)
 
-d <- dist(test, method = "euclidean")
+d <- dist(user_hit, method = "euclidean")
 fit <- hclust(d, method = "ave")
 par(mfrow = c(1,2))
+plot(fit)
 plot(fit, hang = -1)
-rect.hclust(fit, k=6, border="red")
+rect.hclust(fit, k=4, border="red")
+
+d <- dist(user_hit$movie, method = "euclidean")
